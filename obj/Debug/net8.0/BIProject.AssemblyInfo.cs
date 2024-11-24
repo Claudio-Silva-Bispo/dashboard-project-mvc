0a612d1e54307faaa5367fe7c7aedc9cb6a3b71b
Claudio Bispo <claudio.bispo@ezzeseguros.com.br>
2024-11-24 16:04:16 -0300

    FIX - Ocultar página de desenvolvimento e conceito

--- obj/Debug/net8.0/BIProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/BIProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BIProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423830b36c2b21418328958418af73bc18c18d72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+461bdfe991da34a718a819fcdbf08059f104dc8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BIProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BIProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
